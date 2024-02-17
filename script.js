37230866b2bda99e860497d8748d39c764c0352a
Ilyos Nishanov <80672859+ilyos-nishanov@users.noreply.github.com>
2024-02-17 14:21:28 +0500

    addEntry() and some more methods, statements

--- script.js
+++ script.js
@@ -17,6 +17,67 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-  const targetId = '#' + entryDropdown.value;
-  const targetInputContainer = document.querySelector(targetId + ' .input-container');
+  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const HTMLString = `
+  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>`;
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />`;
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>`;
+  
+  /* Step 49
+
+To see your new HTML content for the targetInputContainer, you will need to use the innerHTML property.
+
+The innerHTML property sets or returns the HTML content inside an element.
+
+Here is a form element with a label and input element nested inside.
+
+<form id="form">
+  <label for="first-name">First name</label>
+  <input id="first-name" type="text">
+</form>
+If you want to add another label and input element inside the form, then you can use the innerHTML property as shown below:
+
+const formElement = document.getElementById("form");
+const formContent = `
+  <label for="last-name">Last name</label>
+  <input id="last-name" type="text">
+`;
+formElement.innerHTML += formContent;
+Use the addition assignment operator += to append your HTMLString variable to targetInputContainer.innerHTML.
+*/
+
+targetInputContainer.innerHTML += HTMLString;
 }
+
+/**
+ * Step 50
+
+In the Role Playing Game project, you learned how to set a button's behavior by editing its onclick property. You can also edit an element's behavior by adding an event listener.
+
+The following example uses the addEventListener method to add a click event to a button. When the button is clicked, the printName function is called.
+
+<button class="btn">Print name</button>
+const button = document.querySelector('.btn');
+function printName() {
+  console.log("Jessica");
+}
+button.addEventListener('click', printName);
+The addEventListener method takes two arguments. The first is the event to listen to. (Ex. 'click') The second is the callback function, or the function that runs when the event is triggered.
+
+Call the .addEventListener() method on the addEntryButton. Pass in the string click for the first argument and the addEntry function for the second argument.
+
+Note that you should not call addEntry, but pass the variable (or function reference) directly.
+ */
+
+addEntryButton.addEventListener("click", addEntry);
+
+/*
+Step 52
+
+Your other bug occurs if you add a Breakfast entry, fill it in, then add a second Breakfast entry. You'll see that the values you added disappeared.
+
+This is because you are updating innerHTML directly, which does not preserve your input content. Change your innerHTML assignment to use the insertAdjacentHTML()
+method of targetInputContainer instead. Do not pass any arguments yet.
+*/
+
